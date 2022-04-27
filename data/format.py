b1e03f1d1e0b6b0e34ba78b1782048e15d2fc296
Ali Rostami <rostami@axxessio.com>
2022-04-27 09:30:49 +0200

    correcting the header

--- data/format.py
+++ data/format.py
@@ -5,7 +5,10 @@ with open("persian/persian_A1/persian_A1_sentences_0200_orig.csv") as f:
     spamreader = csv.reader(f, delimiter='\t')
     with open("persian/persian_A1/persian_A1_sentences_0200.csv", "w") as f2:
         fwriter = csv.writer(f2, delimiter='\t')
+        cnt = 0
         for row in spamreader:
-            print(row)
-            fwriter.writerow([row[0], 'سلام! خوبی؟', row[2],  'خوبی', row[4], row[5], row[6], row[7], row[8]])
+            if cnt == 0:
+                fwriter.writerow(row)
+            else:
+                fwriter.writerow([row[0], 'سلام! خوبی؟', row[2],  'خوبی', row[4], row[5], row[6], row[7], row[8]])
             # print(', '.join(row))
