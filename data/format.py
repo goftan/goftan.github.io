7611f591d80a1e39cc09dd723f0f8240c67f2e87
Ali Rostami <rostami@axxessio.com>
2022-04-27 09:26:46 +0200

    adding new file

---
+++ data/format.py
@@ -0,0 +1,11 @@
+import csv
+
+
+with open("persian/persian_A1/persian_A1_sentences_0200_orig.csv") as f:
+    spamreader = csv.reader(f, delimiter='\t')
+    with open("persian/persian_A1/persian_A1_sentences_0200.csv", "w") as f2:
+        fwriter = csv.writer(f2, delimiter='\t')
+        for row in spamreader:
+            print(row)
+            fwriter.writerow([row[0], 'سلام! خوبی؟', row[2],  'خوبی', row[4], row[5], row[6], row[7], row[8]])
+            # print(', '.join(row))
