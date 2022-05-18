f06fd9a01717d08196cb72d3c708ea37ddf1b4bb
Ali Rostami <rostami@axxessio.com>
2022-05-18 11:41:12 +0200

    adding a way of reading

--- public/NewQuiz/index.js
+++ public/NewQuiz/index.js
@@ -5,12 +5,43 @@ function fill_topic_checkboxes() {
     for(var t of avialble_topics) {
        var topic_checkboxes_template = `
         <label class="container">CheckBoxValue
-        <input type="checkbox" checked="checked">
+        <input type="checkbox" checked="checked" value="CheckBoxValue" class="mycheckbox">
         <span class="checkmark"></span>
         </label>`;
-        var topic_checkboxes_replaced = topic_checkboxes_template.replace('CheckBoxValue', t);
+        var topic_checkboxes_replaced = topic_checkboxes_template.replaceAll('CheckBoxValue', t);
         $('#topics_checkboxes').append(topic_checkboxes_replaced)
     } 
 }
 
+function readFileAsText(fileToRead) {
+    return new Promise(function(resolve, reject) {
+        let fr = new FileReader();
+        fr.onloadend = function() {
+            console.log(fr)
+            resolve(fr.result);
+        }
+
+        fr.readAsText(fileToRead);
+    });
+}
+
+
 fill_topic_checkboxes();
+
+let readers = [];
+$('.mycheckbox:checkbox:checked').each(function() {
+    var topic_name = $(this).val().replaceAll(' ','') + '.csv';
+    readers.push($.get(topic_name));
+    // readers.push(readFileAsText(f));
+    // Promise.allSettled(readers).then((values) => {
+        // console.log(values);
+    // }, false);
+});
+
+// var d1 = $.get(...);
+// var d2 = $.get(...);
+
+$.when(readers).done(function(data) {
+    console.log(data);
+    // ...do stuff...
+});
