77c020f7f64f9872b5b2a5cbbfbd7d1aa0c2480d
Ali Rostami <rostami@axxessio.com>
2022-05-18 15:06:25 +0200

    adding a way of reading

--- public/NewQuiz/index.js
+++ public/NewQuiz/index.js
@@ -1,47 +1,46 @@
 var avialble_topics = ['Capitals','Countries','Colors','Animals','Common Words', 'Common Sentences'];
 
 function fill_topic_checkboxes() {
-    var container = document.getElementById('container')
-    for(var t of avialble_topics) {
-       var topic_checkboxes_template = `
-        <label class="container">CheckBoxValue
-        <input type="checkbox" checked="checked" value="CheckBoxValue" class="mycheckbox">
-        <span class="checkmark"></span>
-        </label>`;
-        var topic_checkboxes_replaced = topic_checkboxes_template.replaceAll('CheckBoxValue', t);
-        $('#topics_checkboxes').append(topic_checkboxes_replaced)
-    } 
-}
-
-function readFileAsText(fileToRead) {
-    return new Promise(function(resolve, reject) {
-        let fr = new FileReader();
-        fr.onloadend = function() {
-            console.log(fr)
-            resolve(fr.result);
-        }
-
-        fr.readAsText(fileToRead);
-    });
+    d3.select('#topics_checkboxes').selectAll('label').data(avialble_topics)
+        .enter().append('label').text(function(d){return d;}).attr('class','container')
+        .html(function(d){return '<input type="checkbox" name="topics" value="'+d+'" checked="checked" class="mycheckbox">'
+                                  + d +'<span class="checkmark"></span>';});
+        // .enter().append('input').attr('type', 'checkbox').attr('checked', true).attr('value','CheckBoxValue').attr('class','mycheckbox')
+        // .enter().append('span').attr('class','checkmark');
+    // var container = document.getElementById('container'))
+    // for(var t of avialble_topics) {
+    //    var topic_checkboxes_template = `
+    //     <label class="container">CheckBoxValue
+    //     <input type="checkbox" checked="checked" value="CheckBoxValue" class="mycheckbox">
+    //     <span class="checkmark"></span>
+    //     </label>`;
+    //     var topic_checkboxes_replaced = topic_checkboxes_template.replaceAll('CheckBoxValue', t);
+    //     d3.select('#topics_checkboxes').append('label').style('')
+    // } 
 }
 
 
 fill_topic_checkboxes();
 
-let readers = [];
-$('.mycheckbox:checkbox:checked').each(function() {
-    var topic_name = $(this).val().replaceAll(' ','') + '.csv';
-    readers.push($.get(topic_name));
-    // readers.push(readFileAsText(f));
-    // Promise.allSettled(readers).then((values) => {
-        // console.log(values);
-    // }, false);
+d3.csv('Capitals.csv',function(d) {
+    console.log(d);
 });
 
-// var d1 = $.get(...);
-// var d2 = $.get(...);
+// let readers = [];
+// d3.selectAll('.mycheckbox').property('checked', true).each(function() {
+//     var topic_name = d3.select(this).node().value.replaceAll(' ','') + '.csv';
+//     console.log(topic_name);
+//     readers.push(d3.csv(topic_name));
+// });
 
-$.when(readers).done(function(data) {
-    console.log(data);
-    // ...do stuff...
-});
+
+
+// Promise.all(readers).then(function(files) {
+//     // files[0] will contain file1.csv
+//     // files[1] will contain file2.csv
+//     for(f of files) {
+//         console.log(f);
+//     }
+// }).catch(function(err) {
+//     // handle error here
+// })
