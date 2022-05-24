a3faf8bbdf3ef476b12b2462602e9f05aacef3a2
Ali Rostami <rostami@axxessio.com>
2022-05-24 13:50:23 +0200

    moving js file to js_src dir

---
+++ quiz/js_src/init_nav_menu.js
@@ -0,0 +1,43 @@
+var navs = [
+    'language',
+    'tasks',
+    'question',
+    'puzzle-piece',
+    'calculator',
+    'info'
+]
+
+function click_nav(nav_name) {
+    selectPage(nav_name);
+}
+
+function deselectAllNav() {
+    for(nav of navs) {
+        d3.select('#'+nav+'_page_nav').attr('class','inactive');
+        d3.select('#'+nav+'_page').style("display", 'none');
+    }
+}
+
+function selectPage(page) {
+    deselectAllNav();
+    console.log()
+    d3.select('#'+page+"_nav").attr('class','active');
+    d3.select('#'+page).style("display", 'block');
+}
+
+function init_nav_menu() {
+    d3.select('#navbar').selectAll('a')
+        .data(navs)
+        .enter()
+        .append('a')
+        .attr('id',function(d){ return d+'_page_nav'; })
+        .attr('class',function(d,c){ return c==0 ? 'active' : 'inactive'; })
+        .attr('onclick',function(d){ return 'click_nav(\''+d+'_page\')'; })
+        // .attr('href','javascript:void(0)')
+        .html(function(d){ return '<i class="fa fa-'+d+'"></i>'; });
+}
+
+function init_pages() {
+    
+}
+
