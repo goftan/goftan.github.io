4c246cd5e9dbf341e36a48a88ba3a2abb563a5ec
Ali Rostami <rostami@axxessio.com>
2022-06-01 14:30:40 +0200

    new server

--- quiz/js_src/signin.js
+++ quiz/js_src/signin.js
@@ -1,17 +1,18 @@
 function signin() {
-    d3.json('https://goftan.herokuapp.com/quiz', {
-      method:"POST",
-      body: JSON.stringify({
-        username: document.getElementById('username').value,
-        password: document.getElementById('password').value
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        "Access-Control-Allow-Origin": "*"
-      }
-    })
-    .then(json => {
-        console.log(json);
-    });
+    console.log('here');
+    // d3.json('https://goftan.herokuapp.com/quiz', {
+    //   method:"POST",
+    //   body: JSON.stringify({
+    //     username: document.getElementById('username').value,
+    //     password: document.getElementById('password').value
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+    //     "Access-Control-Allow-Origin": "*"
+    //   }
+    // })
+    // .then(json => {
+    //     console.log(json);
+    // });
 
 }
