c72db6e808edd7bcb575334f9b61e76e8219eb4e
Ali Rostami <rostami@axxessio.com>
2022-06-02 15:59:54 +0200

    bringing dependencies local

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.get('/', (req, res) => {
 
 app.post('/quiz', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log(req);
     user_query = await UserModel.find({user: req.body.username});
     console.log(user_query)
     if(user_query.length !== 0) {
