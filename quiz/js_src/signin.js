c52c43b97bf9fb01d0711872788455a16f6c7880
Ali Rostami <rostami@axxessio.com>
2022-06-02 13:06:03 +0200

    bringing dependencies local

--- quiz/js_src/signin.js
+++ quiz/js_src/signin.js
@@ -1,6 +1,6 @@
 function signin() {
-    if(!validate(d3.select('#username').node().value)) return;
-    if(!d3.select('#password').node().value.length < 5) return;
+    if(!validateEmail(d3.select('#username').node().value)) return;
+    if(d3.select('#password').node().value.length < 5) return;
     d3.json('https://goftan.herokuapp.com/quiz', {
       method:"POST",
       body: JSON.stringify({
@@ -29,22 +29,6 @@ const validateEmail = (email) => {
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
   };
-  
-  const validate = () => {
-      console.log('here');
-    const result = d3.select('#result');
-    const email = d3.select('#email').value();
-    result.text('');
-  
-    if (validateEmail(email)) {
-      result.text(email + ' is valid :)');
-      result.css('color', 'green');
-    } else {
-      result.text(email + ' is not valid :(');
-      result.css('color', 'red');
-    }
-    return false;
-  }
   
 d3.select('#username').on('input', function() {
     d3.select('#result').text('');
