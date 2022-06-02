104a813a10bc6e7d0df74aa55e2513b61666e4ec
Ali Rostami <rostami@axxessio.com>
2022-06-02 08:09:56 +0200

    starting login

--- server.js
+++ server.js
@@ -2,6 +2,26 @@
 const express = require('express');
 var cors = require('cors')
 const app = express();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+const userSchema = new Schema({
+    name:  String, 
+    email: String,
+    pass: String,
+    points:   String,
+    languages: [],
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+const newUser = new User({name: 'test', email: 'test@test.de', pass: 'test', points: '0', languages: []});
+
+
 
 app.use(
     cors({
@@ -21,8 +41,15 @@ db = [];
 app.post('/quiz', (req, res) => {
     console.log(req);
     console.log(res);
+    res = User.find({email: req.body.email, pass: req.body.pass}, function(err, docs) {
+      console.log(err,docs);
+    });
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send({status:'success'});
+    if(res.length !== 0) {
+      res.send({status:'success', message: 'You are logged in', points: res[0].points});
+    } else {
+      res.send({status:'success', message: 'You are not logged in', points: '0'});
+    }
 
 });
 
