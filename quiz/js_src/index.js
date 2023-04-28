55d90193f67215119d85164dcbdd350c2c054327
Ali Rostami <rostami@axxessio.com>
2023-04-28 14:31:52 +0200

    adding chart div

--- quiz/js_src/index.js
+++ quiz/js_src/index.js
@@ -150,7 +150,7 @@ function submitAnswer(which_option) {
 function viewResults() {
     decolorCorrectAnswer(); 
 
-    const xValues = [100,200,300,400,500,600,700,800,900,1000];
+    const xValues = [1,2,3,4,5,6,7,8,9,10];
 
     const xArray = [50,60,70,80,90,100,110,120,130,140,150];
     const yArray = [7,8,8,9,9,9,10,11,14,14,15];
@@ -166,7 +166,7 @@ function viewResults() {
     const layout = {
       xaxis: {range: [40, 160], title: "Square Meters"},
       yaxis: {range: [5, 16], title: "Price in Millions"},  
-      title: "House Prices vs. Size"
+      title: "Learning Progress"
     };
     
     // Display using Plotly
