3f1e22cb092cdc17bdbe001f4686e146ce2d9299
Ali Rostami <rostami@axxessio.com>
2023-04-28 14:28:32 +0200

    adding chart to view results

--- quiz/js_src/index.js
+++ quiz/js_src/index.js
@@ -152,28 +152,25 @@ function viewResults() {
 
     const xValues = [100,200,300,400,500,600,700,800,900,1000];
 
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{ 
-      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-      borderColor: "red",
-      fill: false
-    }, { 
-      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-      borderColor: "green",
-      fill: false
-    }, { 
-      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
-  }
-});
+    const xArray = [50,60,70,80,90,100,110,120,130,140,150];
+    const yArray = [7,8,8,9,9,9,10,11,14,14,15];
+    
+    // Define Data
+    const data = [{
+      x: xArray,
+      y: yArray,
+      mode:"lines"
+    }];
+    
+    // Define Layout
+    const layout = {
+      xaxis: {range: [40, 160], title: "Square Meters"},
+      yaxis: {range: [5, 16], title: "Price in Millions"},  
+      title: "House Prices vs. Size"
+    };
+    
+    // Display using Plotly
+    Plotly.newPlot("myPlot", data, layout);
 
     // if(signined) {
     //     d3.json('https://goftan.herokuapp.com/addresult', {
